099366a8b97aaff5fd32e6c7552a7ff85c43ac17
JumpyJacko <bobjacklee27@gmail.com>
2024-04-10 21:19:13 +1000

    added bufferline :)

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -2,246 +2,424 @@ local conditions = require("heirline.conditions")
 local utils = require("heirline.utils")
 
 local function setup_colours()
-    return {
-        bright_bg = utils.get_highlight("Folded").bg,
-        bright_fg = utils.get_highlight("Folded").fg,
-        red = utils.get_highlight("DiagnosticError").fg,
-        dark_red = utils.get_highlight("DiffDelete").bg,
-        green = utils.get_highlight("DiagnosticOk").fg,
-        blue = utils.get_highlight("Function").fg,
-        gray = utils.get_highlight("NonText").fg,
-        orange = utils.get_highlight("Constant").fg,
-        purple = utils.get_highlight("Conditional").fg,
-        cyan = utils.get_highlight("Special").fg,
-        diag_warn = utils.get_highlight("DiagnosticWarn").fg,
-        diag_error = utils.get_highlight("DiagnosticError").fg,
-        diag_hint = utils.get_highlight("DiagnosticHint").fg,
-        diag_info = utils.get_highlight("DiagnosticInfo").fg,
-        git_del = utils.get_highlight("diffDeleted").fg,
-        git_add = utils.get_highlight("diffAdded").fg,
-        git_change = utils.get_highlight("diffChanged").fg,
-    }
+	return {
+		bright_bg = utils.get_highlight("Folded").bg,
+		bright_fg = utils.get_highlight("Folded").fg,
+		red = utils.get_highlight("DiagnosticError").fg,
+		dark_red = utils.get_highlight("DiffDelete").bg,
+		green = utils.get_highlight("DiagnosticOk").fg,
+		blue = utils.get_highlight("Function").fg,
+		gray = utils.get_highlight("NonText").fg,
+		orange = utils.get_highlight("Constant").fg,
+		purple = utils.get_highlight("Conditional").fg,
+		cyan = utils.get_highlight("Special").fg,
+		diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+		diag_error = utils.get_highlight("DiagnosticError").fg,
+		diag_hint = utils.get_highlight("DiagnosticHint").fg,
+		diag_info = utils.get_highlight("DiagnosticInfo").fg,
+		git_del = utils.get_highlight("diffDeleted").fg,
+		git_add = utils.get_highlight("diffAdded").fg,
+		git_change = utils.get_highlight("diffChanged").fg,
+	}
 end
 
 vim.api.nvim_create_augroup("Heirline", { clear = true })
 vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = function()
-        utils.on_colorscheme(setup_colours)
-    end,
-    group = "Heirline",
+	callback = function()
+		utils.on_colorscheme(setup_colours)
+	end,
+	group = "Heirline",
 })
 
-local ViMode = {
-    init = function(self)
-        self.mode = vim.fn.mode(1)
-    end,
+local Align = { provider = "%=", hl = { bg = "bright_bg" } }
+local Space = { provider = " ", hl = { bg = "bright_bg" } }
 
-    static = {
-        mode_names = { -- change the strings if you like it vvvvverbose!
-            n = "NORMAL",
-            no = "NORMAL",
-            nov = "NORMAL",
-            noV = "NORMAL",
-            ["no\22"] = "NORMAL",
-            niI = "INSERT",
-            niR = "REPLACE",
-            niV = "VISUAL",
-            nt = "TERMINAL",
-            v = "VISUAL",
-            vs = "VISUAL",
-            V = "V-LINE",
-            Vs = "V-BLOCK",
-            ["\22"] = "V-BLOCK",
-            ["\22s"] = "V-BLOCK",
-            s = "SELECT",
-            S = "SELECT LINE",
-            ["\19"] = "SELECT",
-            i = "INSERT",
-            ic = "INSERT",
-            ix = "INSERT",
-            R = "REPLACE",
-            Rc = "REPLACE",
-            Rx = "REPLACE",
-            Rv = "V-REPLACE",
-            Rvc = "V-REPLACE",
-            Rvx = "V-REPLACE",
-            c = "COMMAND",
-            cv = "COMMAND",
-            r = "...",
-            rm = "MORE",
-            ["r?"] = "CONFIRM",
-            ["!"] = "SHELL",
-            t = "TERMINAL",
-        },
-        mode_colours = {
-            n = "blue",
-            i = "green",
-            v = "purple",
-            V = "purple",
-            ["\22"] = "purple",
-            c = "orange",
-            s = "cyan",
-            S = "cyan",
-            ["\19"] = "cyan",
-            R = "orange",
-            r = "orange",
-            ["!"] = "red",
-            t = "red",
-        },
-    },
-    provider = function(self)
-        return "▎ %2("..self.mode_names[self.mode].."%)"
-    end,
-    hl = function(self)
-        local mode = self.mode:sub(1, 1)
-        return { fg = self.mode_colours[mode], bold = true, }
-    end,
-    update = {
-        "ModeChanged",
-        pattern = "*:*",
-        callback = vim.schedule_wrap(function()
-            vim.cmd("redrawstatus")
-        end),
-    },
+-- Status Line Setup
+local ViMode = {
+	init = function(self)
+		self.mode = vim.fn.mode(1)
+	end,
+
+	static = {
+		mode_names = { -- change the strings if you like it vvvvverbose!
+			n = "NORMAL",
+			no = "NORMAL",
+			nov = "NORMAL",
+			noV = "NORMAL",
+			["no\22"] = "NORMAL",
+			niI = "INSERT",
+			niR = "REPLACE",
+			niV = "VISUAL",
+			nt = "TERMINAL",
+			v = "VISUAL",
+			vs = "VISUAL",
+			V = "V-LINE",
+			Vs = "V-BLOCK",
+			["\22"] = "V-BLOCK",
+			["\22s"] = "V-BLOCK",
+			s = "SELECT",
+			S = "SELECT LINE",
+			["\19"] = "SELECT",
+			i = "INSERT",
+			ic = "INSERT",
+			ix = "INSERT",
+			R = "REPLACE",
+			Rc = "REPLACE",
+			Rx = "REPLACE",
+			Rv = "V-REPLACE",
+			Rvc = "V-REPLACE",
+			Rvx = "V-REPLACE",
+			c = "COMMAND",
+			cv = "COMMAND",
+			r = "...",
+			rm = "MORE",
+			["r?"] = "CONFIRM",
+			["!"] = "SHELL",
+			t = "TERMINAL",
+		},
+		mode_colours = {
+			n = "blue",
+			i = "green",
+			v = "purple",
+			V = "purple",
+			["\22"] = "purple",
+			c = "orange",
+			s = "cyan",
+			S = "cyan",
+			["\19"] = "cyan",
+			R = "orange",
+			r = "orange",
+			["!"] = "red",
+			t = "red",
+		},
+	},
+	provider = function(self)
+		return "▎ %2(" .. self.mode_names[self.mode] .. "%)"
+	end,
+	hl = function(self)
+		local mode = self.mode:sub(1, 1)
+		return { fg = self.mode_colours[mode], bold = true }
+	end,
+	update = {
+		"ModeChanged",
+		pattern = "*:*",
+		callback = vim.schedule_wrap(function()
+			vim.cmd("redrawstatus")
+		end),
+	},
 }
 
 local Git = {
-    condition = conditions.is_git_repo,
+	condition = conditions.is_git_repo,
 
-    init = function(self)
-        self.status_dict = vim.b.gitsigns_status_dict
-        self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
-    end,
+	init = function(self)
+		self.status_dict = vim.b.gitsigns_status_dict
+		self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
+	end,
 
-    hl = { fg = "green" },
+	hl = { fg = "green" },
 
-    {   -- git branch name
-        provider = function(self)
-            return " " .. self.status_dict.head
-        end,
-        hl = { bold = true }
-    },
+	{ -- git branch name
+		provider = function(self)
+			return " " .. self.status_dict.head
+		end,
+		hl = { bold = true },
+	},
 }
 
 local Ruler = {
-    -- %l = current line number
-    -- %L = number of lines in the buffer
-    -- %c = column number
-    -- %P = percentage through file of displayed window
-    provider = "%7(%l/%3L%):%2c %P",
-    hl = { bg = "bright_bg" },
+	-- %l = current line number
+	-- %L = number of lines in the buffer
+	-- %c = column number
+	-- %P = percentage through file of displayed window
+	provider = "%7(%l/%3L%):%2c %P",
+	hl = { bg = "bright_bg" },
 }
 
-local ScrollBar ={
-    static = {
-        sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' }
-        -- Another variant, because the more choice the better.
-        -- sbar = { '🭶', '🭷', '🭸', '🭹', '🭺', '🭻' }
-    },
-    provider = function(self)
-        local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-        local lines = vim.api.nvim_buf_line_count(0)
-        local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
-        return string.rep(self.sbar[i], 2)
-    end,
-    hl = { fg = "blue", bg = "bright_bg" },
+local ScrollBar = {
+	static = {
+		sbar = { "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" },
+		-- Another variant, because the more choice the better.
+		-- sbar = { '🭶', '🭷', '🭸', '🭹', '🭺', '🭻' }
+	},
+	provider = function(self)
+		local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+		local lines = vim.api.nvim_buf_line_count(0)
+		local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+		return string.rep(self.sbar[i], 2)
+	end,
+	hl = { fg = "blue", bg = "bright_bg" },
 }
 
 local LSPActive = {
-    condition = conditions.lsp_attached,
-    update = {'LspAttach', 'LspDetach'},
+	condition = conditions.lsp_attached,
+	update = { "LspAttach", "LspDetach" },
 
-    -- You can keep it simple,
-    -- provider = " [LSP]",
+	-- You can keep it simple,
+	-- provider = " [LSP]",
 
-    -- Or complicate things a bit and get the servers names
-    provider  = function()
-        local names = {}
-        for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-            table.insert(names, server.name)
-        end
-        return table.concat(names, " ")
-    end,
-    hl = { fg = "green", bold = true },
+	-- Or complicate things a bit and get the servers names
+	provider = function()
+		local names = {}
+		for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+			table.insert(names, server.name)
+		end
+		return table.concat(names, " ")
+	end,
+	hl = { fg = "green", bold = true },
 }
 
 local FileNameBlock = {
-    -- let's first set up some attributes needed by this component and it's children
-    init = function(self)
-        self.filename = vim.api.nvim_buf_get_name(0)
-    end,
+	-- let's first set up some attributes needed by this component and it's children
+	init = function(self)
+		self.filename = vim.api.nvim_buf_get_name(0)
+	end,
 }
 
 local FileFlags = {
-    {
-        condition = function()
-            return vim.bo.modified
-        end,
-        provider = "[+]",
-        hl = { fg = "green" },
-    },
-    {
-        condition = function()
-            return not vim.bo.modifiable or vim.bo.readonly
-        end,
-        provider = "",
-        hl = { fg = "orange" },
-    },
+	{
+		condition = function()
+			return vim.bo.modified
+		end,
+		provider = "[+]",
+		hl = { fg = "green" },
+	},
+	{
+		condition = function()
+			return not vim.bo.modifiable or vim.bo.readonly
+		end,
+		provider = "",
+		hl = { fg = "orange" },
+	},
 }
 
 local FileType = {
-    provide = function()
-        return string.upper(vim.bo.filetype)
-    end,
-    hl = { fg = utils.get_highlight("Type").fg, bold = true },
+	provide = function()
+		return string.upper(vim.bo.filetype)
+	end,
+	hl = { fg = utils.get_highlight("Type").fg, bold = true },
 }
 
 local FileName = {
-    init = function(self)
-        self.lfilename = vim.fn.fnamemodify(self.filename, ":.")
-        if self.lfilename == "" then self.lfilename = "[No Name]" end
-    end,
-    hl = { fg = utils.get_highlight("Directory").fg },
+	init = function(self)
+		self.lfilename = vim.fn.fnamemodify(self.filename, ":.")
+		if self.lfilename == "" then
+			self.lfilename = "[No Name]"
+		end
+	end,
+	hl = { fg = utils.get_highlight("Directory").fg },
 
-    flexible = 2,
+	flexible = 2,
 
-    {
-        provider = function(self)
-            return self.lfilename
-        end,
-    },
-    {
-        provider = function(self)
-            return vim.fn.pathshorten(self.lfilename)
-        end,
-    },
+	{
+		provider = function(self)
+			return self.lfilename
+		end,
+	},
+	{
+		provider = function(self)
+			return vim.fn.pathshorten(self.lfilename)
+		end,
+	},
 }
 
 local FileNameModifer = {
-    hl = function()
-        if vim.bo.modified then
-            -- use `force` because we need to override the child's hl foreground
-            return { fg = "cyan", bold = true, force=true }
-        end
-    end,
+	hl = function()
+		if vim.bo.modified then
+			-- use `force` because we need to override the child's hl foreground
+			return { fg = "cyan", bold = true, force = true }
+		end
+	end,
 }
 
-local Align = { provider = "%=", hl = { bg = "bright_bg" } }
-local Space = { provider = " ", hl = {bg = "bright_bg"} }
+-- Buffer Line Setup
+local TablineBufnr = {
+	provider = function(self)
+		return tostring(self.bufnr) .. ". "
+	end,
+	hl = "Comment",
+}
 
-FileNameBlock = utils.insert(
-    FileNameBlock,
-    utils.insert(FileNameModifer, FileName),
-    FileFlags,
-    { provider = '%<'}
+local TablineFileName = {
+	provider = function(self)
+		local filename = self.filename
+		filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+		return filename
+	end,
+	hl = function(self)
+		return { fg = "gray", bold = self.is_active or self.is_visible, italic = true }
+	end,
+}
+
+local TablineFileFlags = {
+	{
+		condition = function(self)
+			return vim.api.nvim_get_option_value("modified", { buf = self.bufnr })
+		end,
+		provider = "[+]",
+		hl = { fg = "green" },
+	},
+	{
+		condition = function(self)
+			return not vim.api.nvim_get_option_value("modifiable", { buf = self.bufnr })
+				or vim.api.nvim_get_option_value("readonly", { buf = self.bufnr })
+		end,
+		provider = function(self)
+			if vim.api.nvim_get_option_value("buftype", { buf = self.bufnr }) == "terminal" then
+				return "  "
+			else
+				return ""
+			end
+		end,
+		hl = { fg = "orange" },
+	},
+}
+
+local TablineFileNameBlock = {
+	init = function(self)
+		self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+	end,
+	hl = function(self)
+		if self.is_active then
+			return "TabLineSel"
+		else
+			return "TabLine"
+		end
+	end,
+	on_click = {
+		callback = function(_, minwid, _, button)
+			if button == "m" then
+				vim.schedule(function()
+					vim.api.nvim_buf_delete(minwid, { force = false })
+				end)
+			else
+				vim.api.nvim_win_set_buf(0, minwid)
+			end
+		end,
+		minwid = function(self)
+			return self.bufnr
+		end,
+		name = "heirline_tabline_buffer_callback",
+	},
+	-- TablineBufnr,
+	TablineFileName,
+	TablineFileFlags,
+}
+
+-- a nice "x" button to close the buffer
+local TablineCloseButton = {
+	condition = function(self)
+		return not vim.api.nvim_get_option_value("modified", { buf = self.bufnr })
+	end,
+	{ provider = " " },
+	{
+		provider = "",
+		hl = { fg = "gray" },
+		on_click = {
+			callback = function(_, minwid)
+				vim.schedule(function()
+					vim.api.nvim_buf_delete(minwid, { force = false })
+					vim.cmd.redrawtabline()
+				end)
+			end,
+			minwid = function(self)
+				return self.bufnr
+			end,
+			name = "heirline_tabline_close_buffer_callback",
+		},
+	},
+}
+
+-- The final touch!
+local TablineBufferBlock = utils.surround({ "", "" }, function(self)
+	if self.is_active then
+		return utils.get_highlight("TabLineSel").bg
+	else
+		return utils.get_highlight("TabLine").bg
+	end
+end, { TablineFileNameBlock, TablineCloseButton })
+
+-- and here we go
+local BufferLine = utils.make_buflist(
+	TablineBufferBlock,
+	{ provider = "", hl = { fg = "gray" } }, -- left truncation, optional (defaults to "<")
+	{ provider = "", hl = { fg = "gray" } } -- right trunctation, also optional (defaults to ...... yep, ">")
+	-- by the way, open a lot of buffers and try clicking them ;)
 )
 
-local StatusLine = { ViMode, Space, Space, Git, Space, Space, FileNameBlock,
-                     Align, LSPActive, Space, FileType, Space, Ruler, Space, ScrollBar }
+-- this is the default function used to retrieve buffers
+local get_bufs = function()
+	return vim.tbl_filter(function(bufnr)
+		return vim.api.nvim_get_option_value("buflisted", { buf = bufnr })
+	end, vim.api.nvim_list_bufs())
+end
 
-require("heirline").setup {
-    statusline = StatusLine,
-    opts = {
-        colors = setup_colours,
-    },
+-- initialize the buflist cache
+local buflist_cache = {}
+
+-- setup an autocmd that updates the buflist_cache every time that buffers are added/removed
+vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
+	callback = function()
+		vim.schedule(function()
+			local buffers = get_bufs()
+			for i, v in ipairs(buffers) do
+				buflist_cache[i] = v
+			end
+			for i = #buffers + 1, #buflist_cache do
+				buflist_cache[i] = nil
+			end
+
+			-- check how many buffers we have and set showtabline accordingly
+			if #buflist_cache > 1 then
+				vim.o.showtabline = 2 -- always
+			elseif vim.o.showtabline ~= 1 then -- don't reset the option if it's already at default value
+				vim.o.showtabline = 1 -- only when #tabpages > 1
+			end
+		end)
+	end,
+})
+
+local BufferLine = utils.make_buflist(
+	TablineBufferBlock,
+	{ provider = " ", hl = { fg = "gray" } },
+	{ provider = " ", hl = { fg = "gray" } },
+	-- out buf_func simply returns the buflist_cache
+	function()
+		return buflist_cache
+	end,
+	-- no cache, as we're handling everything ourselves
+	false
+)
+
+FileNameBlock = utils.insert(FileNameBlock, utils.insert(FileNameModifer, FileName), FileFlags, { provider = "%<" })
+
+local StatusLine = {
+	ViMode,
+	Space,
+	Space,
+	Git,
+	Space,
+	Space,
+	FileNameBlock,
+	Align,
+	LSPActive,
+	Space,
+	FileType,
+	Space,
+	Ruler,
+	Space,
+	ScrollBar,
 }
+
+local TabLine = { BufferLine }
+
+require("heirline").setup({
+	statusline = StatusLine,
+	tabline = TabLine,
+	opts = {
+		colors = setup_colours,
+	},
+})
+
+vim.o.showtabline = 2
+vim.cmd([[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]])
