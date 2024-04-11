a30cfad130774cbf1176c489075f69aa5c60dc93
JumpyJacko <bobjacklee27@gmail.com>
2024-04-11 22:47:43 +1000

    Fix diagnostic signs not loading in time + started using vim.loader.enable()

--- lua/options.lua
+++ lua/options.lua
@@ -23,3 +23,9 @@ vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#3B4252", fg = "#5E81AC" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3B4252" })
 vim.api.nvim_set_hl(0, "TelescopeNormal", {})
 vim.api.nvim_set_hl(0, "TelescopeBorder", {})
+
+-- Better Diagnostic Signs
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,3 @@
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-
 local border_style = { " ", " ", " ", " ", " ", " ", " ", " " }
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "VisualNC" })
 
