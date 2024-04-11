743af99c2be6b280b82ae9938443cbb4b2faf1dd
JumpyJacko <bobjacklee27@gmail.com>
2024-04-11 22:12:34 +1000

    Fix up colours + add diagnostics to statusline

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -207,7 +207,7 @@ local FileName = {
 			self.lfilename = "[No Name]"
 		end
 	end,
-	hl = { fg = utils.get_highlight("Directory").fg },
+	hl = { fg = "gray" },
 
 	flexible = 2,
 
@@ -247,7 +247,11 @@ local TablineFileName = {
 		return filename
 	end,
 	hl = function(self)
-		return { fg = "gray", bold = self.is_active or self.is_visible, italic = true }
+        local colour = "gray"
+        if self.is_active then
+            colour = "cyan"
+        end
+		return { fg = colour, bold = self.is_active or self.is_visible, italic = true }
 	end,
 }
 
@@ -331,12 +335,7 @@ local TablineCloseButton = {
 }
 
 -- The final touch!
-local TablineBufferBlock = utils.surround({ "", "" }, function(self)
-	if self.is_active then
-		return utils.get_highlight("TabLineSel").bg
-	else
-		return utils.get_highlight("TabLine").bg
-	end
+local TablineBufferBlock = utils.surround({ " ", " " }, function(self)
 end, { TablineFileNameBlock, TablineCloseButton })
 
 -- and here we go
@@ -377,6 +376,53 @@ local TabLineOffset = {
     end,
 }
 
+local Diagnostics = {
+
+    condition = conditions.has_diagnostics,
+
+    static = {
+        error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+    },
+
+    init = function(self)
+        self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+        self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+        self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+        self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+    end,
+
+    update = { "DiagnosticChanged", "BufEnter" },
+
+    {
+        provider = function(self)
+            -- 0 is just another output, we can decide to print it or not!
+            return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+        end,
+        hl = { fg = "diag_error" },
+    },
+    {
+        provider = function(self)
+            return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+        end,
+        hl = { fg = "diag_warn" },
+    },
+    {
+        provider = function(self)
+            return self.info > 0 and (self.info_icon .. self.info .. " ")
+        end,
+        hl = { fg = "diag_info" },
+    },
+    {
+        provider = function(self)
+            return self.hints > 0 and (self.hint_icon .. self.hints)
+        end,
+        hl = { fg = "diag_hint" },
+    },
+}
+
 FileNameBlock = utils.insert(FileNameBlock, utils.insert(FileNameModifer, FileName), FileFlags, { provider = "%<" })
 
 local StatusLine = {
@@ -388,6 +434,8 @@ local StatusLine = {
 	Space,
 	FileNameBlock,
 	Align,
+    Diagnostics,
+    Space,
 	LSPActive,
 	Space,
 	FileType,
