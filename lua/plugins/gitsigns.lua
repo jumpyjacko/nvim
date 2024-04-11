c68408b3e0559696905154cf28f1c58b27d07d78
JumpyJacko <bobjacklee27@gmail.com>
2024-04-11 21:31:11 +1000

    Fix up colours for gitsigns + make lsp less annoying

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,8 +13,8 @@ require("gitsigns").setup {
 local colours = {
     bg = vim.fn.synIDattr(vim.fn.hlID('Folded'), 'bg'),
     green = vim.fn.synIDattr(vim.fn.hlID('DiagnosticOk'), 'fg'),
-    orange = vim.fn.synIDattr(vim.fn.hlID('Constant'), 'fg'),
-    red = vim.fn.synIDattr(vim.fn.hlID('DiagnosticError'), 'fg'),
+    orange = vim.fn.synIDattr(vim.fn.hlID('Tag'), 'fg'),
+    red = vim.fn.synIDattr(vim.fn.hlID('Constant'), 'fg'),
 }
 
 vim.api.nvim_set_hl(0, "DiffAdd", { bg = colours.bg, fg = colours.green })
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,22 +1,32 @@
-require("mason").setup {}
-require("mason-lspconfig").setup {
-    ensure_installed = { "clangd", "lua_ls", "marksman", "pyright", "tsserver", "rust_analyzer", "zls" }
-}
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "clangd", "lua_ls", "marksman", "pyright", "tsserver", "rust_analyzer", "zls" },
+})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("mason-lspconfig").setup_handlers {
-    function(server_name)
-        require("lspconfig")[server_name].setup({
-            capabilities = capabilities
-        })
-    end,
-    ["rust_analyzer"] = function() end,
-}
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				-- Disable displaying LSP diagnostics in the gutter
+				vim.lsp.handlers["textDocument/publishDiagnostics"] =
+					vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+						update_in_insert = false, -- Disable updating diagnostics during insert mode
+						virtual_text = true, -- Disable virtual text (inline messages)
+						signs = false, -- Enable signs (e.g., error signs in the gutter)
+						underline = false, -- Disable underline for diagnostics
+					})
+			end,
+		})
+	end,
+	["rust_analyzer"] = function() end,
+})
 
-require("null-ls").setup {}
-require("mason-null-ls").setup {
-    ensure_installed = { "stylua" },
-    automatic_installtion = false,
-    handlers = {}
-}
+require("null-ls").setup({})
+require("mason-null-ls").setup({
+	ensure_installed = { "stylua" },
+	automatic_installtion = false,
+	handlers = {},
+})
