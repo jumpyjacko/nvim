9dd1857e75492a7aaf7742cfb6a50e7037054437
JumpyJacko <bobjacklee27@gmail.com>
2024-04-13 16:03:04 +1000

    Remove unwanted keybinds

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -68,24 +68,7 @@ require("neo-tree").setup({
             ["?"] = "show_help",
             ["<"] = "prev_source",
             [">"] = "next_source",
-            ["h"] = function(state)
-                local node = state.tree:get_node()
-                if node.type == 'directory' and node:is_expanded() then
-                    require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
-                else
-                    require 'neo-tree.ui.renderer'.focus_node(state, node:get_parent_id())
-                end
-            end,
-            ["i"] = function(state)
-                local node = state.tree:get_node()
-                if node.type == 'directory' then
-                    if not node:is_expanded() then
-                        require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
-                    elseif node:has_children() then
-                        require 'neo-tree.ui.renderer'.focus_node(state, node:get_child_ids()[1])
-                    end
-                end
-            end,
+            ["i"] = "show_file_details",
         },
     },
     filesystem = {
