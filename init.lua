79c5abe01fa751e7f914f86ee27dea79d01603c0
JumpyJacko <bobjacklee27@gmail.com>
2024-06-24 13:43:13 +1000

    fix: Keybind mapping + truely lazyloading crates.nvim

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
 require("core.lazy")
-require("core.keymaps")
+require("core.mappings")
 require("core.options")
 require("core.autocommands")
--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -3,11 +3,29 @@ return {
     tag = "stable",
     event = { "BufRead Cargo.toml" },
     config = function()
-        require('crates').setup({
+        require("crates").setup({
             null_ls = {
                 enabled = true,
                 name = "crates.nvim",
             },
         })
+
+        local crates = require("crates")
+        
+        Map("n", "<leader>rt", crates.toggle, { desc = "Toggle crates.nvim" })
+        Map("n", "<leader>rr", crates.reload, { desc = "Reload crates.nvim" })
+        
+        Map("n", "<leader>rf", crates.show_features_popup, { desc = "Show features popup" })
+        Map("n", "<leader>rd", crates.show_dependencies_popup, { desc = "Show dependencies popup" })
+
+        Map("n", "<leader>ru", crates.update_crate, { desc = "Update crate" })
+        Map("v", "<leader>ru", crates.update_crates, { desc = "Update crates" })
+        Map("n", "<leader>ra", crates.update_all_crates, { desc = "Update all crates" })
+        Map("n", "<leader>ru", crates.upgrade_crate, { desc = "Upgrade crate" })
+        Map("v", "<leader>rU", crates.upgrade_crates, { desc = "Upgrade crates" })
+        Map("n", "<leader>rA", crates.upgrade_all_crates, { desc = "Upgrade all crates" })
+
+        Map("n", "<leader>rx", crates.expand_plain_crate_to_inline_table, { desc = "Expand crate to inline table" })
+        Map("n", "<leader>rX", crates.extract_crate_into_table, { desc = "Extract crate into table" })
     end,
 }
