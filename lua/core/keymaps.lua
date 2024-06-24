d35819259f63996643a46183cb574289c342f921
JumpyJacko <bobjacklee27@gmail.com>
2024-06-24 13:33:34 +1000

    fix: Remove the annoying 'keymaps.lua: ' from keymap desc

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,6 @@
 local function map(mode, lhs, rhs, opts)
 	local options = { noremap = true, silent = true, nowait = true }
 	if opts then
-		if opts.desc then
-			opts.desc = "keymaps.lua: " .. opts.desc
-		end
 		options = vim.tbl_extend("force", options, opts)
 	end
 	vim.keymap.set(mode, lhs, rhs, options)
