d48703ab0f6416b15fc0a56a05e32a2f2f601ebd
JumpyJacko <bobjacklee27@gmail.com>
2025-08-10 11:37:47 +1000

    add: treesitter but i actually turn it on

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ vim.pack.add({
     { src = "https://github.com/echasnovski/mini.pick" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/windwp/nvim-autopairs" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
@@ -46,6 +47,7 @@ require "nvim-autopairs".setup()
 require "mason".setup()
 require "mason-lspconfig".setup()
 require "oil".setup()
+require "nvim-treesitter.configs".setup({ highlight = { enable = true } })
 require "blink.cmp".setup({
     completion = {
         documentation = { auto_show = true },
